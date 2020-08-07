68607698bb0dd6e1ba1e2999916c1e1033486c63
yukiej <yukijung@google.com>
2020-08-07 12:39:33 -0400

    Check if preview has already completed before calling stop preview endpoint on destroy and add silent mode for stopPreview

--- cdap-ui/app/hydrator/controllers/create/toppanel-ctrl.js
+++ cdap-ui/app/hydrator/controllers/create/toppanel-ctrl.js
@@ -143,7 +143,7 @@ class HydratorPlusPlusTopPanelCtrl {
 
     $scope.$on('$destroy', () => {
       unsub();
-      this.stopPreview();
+      this.stopPreview(true);
       this.previewStore.dispatch(
         this.previewActions.togglePreviewMode(false)
       );
@@ -648,8 +648,8 @@ class HydratorPlusPlusTopPanelCtrl {
       });
   }
 
-  stopPreview() {
-    if (!this.currentPreviewId) {
+  stopPreview(silentMode = false) {
+    if (!this.currentPreviewId || !this.previewRunning || this.loadingLabel === 'Stopping') {
       return;
     }
     let params = {
@@ -671,6 +671,9 @@ class HydratorPlusPlusTopPanelCtrl {
             this.dataSrc.stopPoll(this.pollId);
             this.pollId = null;
 
+            if (silentMode) {
+              return;
+            }
             let pipelinePreviewPlaceholder = 'The preview of the pipeline';
             const pipelineName = this.HydratorPlusPlusConfigStore.getName();
             if (pipelineName.length > 0) {
@@ -684,6 +687,9 @@ class HydratorPlusPlusTopPanelCtrl {
           (err) => {
             this.previewLoading = false;
             this.previewRunning = true;
+            if (silentMode) {
+              return;
+            }
             this.myAlertOnValium.show({type: 'danger', content: err.data});
         });
   }
